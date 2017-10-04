b86bfb2aea06d01c1d7a55e2343fae7e80574756
Anderson Bruns <andrafaelb@gmail.com>
2017-10-03 21:23:33 -0300

    Final

---
+++ DB/UltimoDados.sql
@@ -0,0 +1,24 @@
+﻿INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (3,'(47)3022-5489','José da Silva','2018-10-22','BGC','1ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (5,'(47)3320-1212','Elias Renato Henrique Monteiro','2018-09-06','Vacina contra Meningococo C','2ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (6,'(47)3695-8221','Elias Lorenzo Alves','2017-11-03','Dupla Adulto','2ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (7,'(47)3459-8655','Paulo Bryan Monteiro','2019-06-02','Dupla Adulto','3ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (8,'(47)9925-6521','Vitor Eduardo André Castro','2020-05-30','Hepatite B','1ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (9,'(47)3365-8955','Arthur Bryan da Silva','2020-02-12','Tríplice Viral ','Dose única');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (10,'(47)9852-0212','Daniel Enzo Gomes','2019-03-09','Vacina Oral contra a Pólio','2ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (11,'(47)3201-1547','Kaique Erick Cardoso','2019-11-20','Influenza','Dose única');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (12,'(47)3326-5980','Cauê Thales Pietro Dias','2019-12-08','Vacina contra Pneumococos','1ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (13,'(47)8896-5233','Miguel Daniel Felipe Carvalho','2020-03-03','Vacina Tetravalente','2ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (14,'(47)8965-4125','Vinicius Rodrigo Breno Oliveira','2017-12-03','Vacina Oral contra a Pólio','3ª dose');
+GO
+INSERT INTO "PROX_VACINA" ("ID","SUS_CODIGO","NOME","DATA_RETORNO","VACINA_RETORNO","DOSE") VALUES (15,'(47)3458-9620','Thiago Theo Monteiro','2018-05-12','Vacina contra Pneumococos','1ª dose');
+GO
